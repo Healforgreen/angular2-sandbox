e190503b4ff3e5fa5376ac3dd0cc6c71b97ddeb9
Elastic Cloud User <dc-user@ech-10-157-131-143.mastercard.int>
2016-03-24 15:10:25 -0500

    added small well component

---
+++ src/well/small.well.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+Component({
+  selector: "small-well",
+  templateUrl: "src/well/small.well.component.html",
+  styleUrls: ["src/well/small.well.component.css"]
+})
+export class SmallWell {
+  
+}
