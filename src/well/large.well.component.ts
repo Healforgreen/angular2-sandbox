2f6d93d504c8f7964efb11d5eb00c79237453b70
Elastic Cloud User <dc-user@ech-10-157-131-143.mastercard.int>
2016-03-24 15:01:53 -0500

    added large well component

---
+++ src/well/large.well.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: "large-well",
+  templateUrl: "src/well/large.well.component.html",
+  styleUrls: ["src/well/large.well.component.css"]
+})
+export class LargeWell {
+  
+}
